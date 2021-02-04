2f2a9e1add8d4cab547995c0b66057960670e359
simfrog <ism0705@naver.com>
2021-02-04 17:54:39 +0900

    2021-02-04

---
+++ Algorithm/16_Dynamic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int d[100];
+
+int fibonacci(int x) {
+    if(x == 1) return 1;
+    if(x == 2) return 1;
+    if(d[x] != 0) return d[x];
+    return d[x] = fibonacci(x-1) + fibonacci(x-2);
+}
+
+int main()
+{
+    cout << fibonacci(30);
+    return 0;
+}
